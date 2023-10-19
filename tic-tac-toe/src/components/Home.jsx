6f72d2fbdc0fa330b101b9f7e30d5ef4bf403302
AGNES LILY <142089703+agneslily4709@users.noreply.github.com>
2023-10-19 23:39:52 +0530

    Update Home.jsx

--- tic-tac-toe/src/components/Home.jsx
+++ tic-tac-toe/src/components/Home.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Cell = () => {
+const Cell = ({firstPlayer,setFirstPlayer}) => {
         return (
-                <div>h1</div>
+                <div className='cell' onClick={()=>setFirstPlayer((prev)=>!prev)}>
+                        {firstPlayer ? "Player 1":"Player 2"}
+                </div>
         )
 }
 
 const Home = () => {
+        const [cellText,setCellText] = useState("")
+        const [firstPlayer,setFirstPlayer] = useState(false)
+        const cellArray = [1,2,3,4,5,6,7,8,9]
   return (
     <div className='home'>
         <div className='playground'>
@@ -15,7 +20,13 @@ const Home = () => {
                         <h1>Player 2 turn</h1>
                 </div>
                 <div className='board'>
-                        
+                        {cellArray.map((cell)=>
+                                (
+                                        <div className='cell' onClick={()=>setFirstPlayer((prev)=>!prev)}>
+                                                {firstPlayer ? "Player 1":"Player 2"}
+                                        </div>
+                                )
+                        )}
                 </div>
         </div>
         <div className='results'>
