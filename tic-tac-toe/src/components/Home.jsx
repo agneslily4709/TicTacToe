e83e35f1a24c87355adc4d7c1a64079ce57dc846
AGNES LILY <142089703+agneslily4709@users.noreply.github.com>
2023-10-19 23:31:05 +0530

    Update Home.jsx

--- tic-tac-toe/src/components/Home.jsx
+++ tic-tac-toe/src/components/Home.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 
+const Cell = () => {
+        return (
+                <div>h1</div>
+        )
+}
+
 const Home = () => {
   return (
     <div className='home'>
         <div className='playground'>
-
+                <div className='notification'>
+                        <h1>Player 1 turn</h1>
+                        <h1>Player 2 turn</h1>
+                </div>
+                <div className='board'>
+                        
+                </div>
         </div>
         <div className='results'>
 
